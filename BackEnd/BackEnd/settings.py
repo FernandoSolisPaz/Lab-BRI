e253605dab82b74e2d2afe05457ea5b360844819
wittyZzZ <william.zubarzo@usach.cl>
2025-07-14 22:29:29 -0400

    Backend CORS fix

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -37,18 +37,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'typesenseapp.apps.TypesenseappConfig'
+    'typesenseapp.apps.TypesenseappConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'BackEnd.urls'
 
