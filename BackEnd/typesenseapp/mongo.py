df3925e0154cfebc14e88f647520e14a395a33fc
lemonpatt <patricio.briceno@usach.cl>
2025-07-16 21:32:19 -0400

    fix con pymongo

--- BackEnd/typesenseapp/mongo.py
+++ BackEnd/typesenseapp/mongo.py
@@ -1,8 +1,9 @@
-from mongoengine import connect
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 mongo_uri = os.getenv('MONGO_URI')
-conn = connect(host=mongo_uri)
+conn = MongoClient(mongo_uri, server_api=ServerApi('1'))
